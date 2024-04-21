18e71af39be12010ede5085fea143c1e90ee0f94
MIRICMILAN <104166918+MIRICMILAN@users.noreply.github.com>
2024-04-21 13:06:21 -0700

    test code v1

---
+++ CoralTestCode[V1].py
@@ -0,0 +1,12 @@
+
+# Prerequisite Packages # 
+import os
+import pathlib
+from pycoral.utils import edgetpu
+from pycoral.utils import dataset
+from pycoral.adapters import common
+from pycoral.adapters import classify
+from PIL import Image
+
+
+print("Hello World")
